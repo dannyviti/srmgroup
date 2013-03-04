433f0a587e15b96e37715230a9aa8014be2c8385
Danny Viti <dviti@area-milan.(none)>
2013-03-03 20:39:46 -0500

    loss form rd 1

--- customer_portal/admin.py
+++ customer_portal/admin.py
@@ -5,7 +5,26 @@ from adminfiles.admin import FilePickerAdmin
 from srmgroup.customer_portal.models import *
 
 class FilesAdmin(FilePickerAdmin):
-	adminfiles_fields = ['file_inc',]
+    adminfiles_fields = ['file_inc',]
+
+class responder_informationAdmin(admin.ModelAdmin):
+    list_diplay = ('contractor_name','city','state')
+
+class loss_reportAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,           {'fields': ['date','srm_id','reported_by','o_call_back','c_call_back','f_call_back','organization']}),
+        (None,           {'fields': ['loss_loc','site_cont','material_dis','material_dis_qt']}),
+        ('Released to:', {'fields': ['pervious','impervious','surface_water','storm_drain']}),
+        (None,           {'fields': ['msds_sec','dot_reprtbl']}),
+        ('Client Reference Number (Obtain one of the follwing)', {'fields': ['c_claim_no','c_freight_no','c_adjuster','c_tractor_no','c_trailer_no','c_driver','c_driver_no']}),
+        (None,           {'fields': ['police_fire_recovery']}),
+        ('Agency Notification',           {'fields': ['agency_state','agency_state_no','agency_epa','agency_epa_no','agency_nrc','agency_nrc_no']}),
+        (None,           {'fields': ['responder_info']}),
+        (None,           {'fields': ['initial_corrective_actions','intial_reserve','ir_given_to','ir_date','adjusted_reserve','ar_given_to','ar_date','project_scope_timeline','site_work_completed_comments']}),
+    ]
+    list_display = ('srm_id','organization','date')
 
 admin.site.register(Files, FilesAdmin)
+admin.site.register(responder_information, responder_informationAdmin)
+admin.site.register(loss_report, loss_reportAdmin)
 
--- customer_portal/models.py
+++ customer_portal/models.py
@@ -11,8 +11,68 @@ class Files(models.Model):
 	def __unicode__(self):
 		return self.name
 	class Meta:
-		verbose_name_plural = "Files"
+		verbose_name_plural = 'Files'
 		permissions = (
 			("can_view_uploads", "Can view uploaded files"),
 		)
+
+class responder_information(models.Model):
+	contractor_name = models.CharField(max_length=100)
+	city = models.CharField(max_length=100)
+	state = models.CharField(max_length=100)
+	phone = models.CharField(max_length=100)
+	fax = models.CharField(max_length=100)
+	email = models.CharField(max_length=100)
+	website = models.CharField(max_length=100)
+	def __unicode__(self):
+		return u'%s (%s, %s)' % (self.contractor_name, self.city, self.state)
+	class Meta:
+		verbose_name = 'Responder Information'
+		verbose_name_plural = 'Responders Information'
+
+class loss_report(models.Model):
+	date = models.DateTimeField()
+	srm_id = models.CharField('SRM ID', max_length=50)
+	reported_by = models.CharField('Name of Person Calling', max_length=200)
+	o_call_back = models.CharField('Office No.', max_length=20)
+	c_call_back = models.CharField('Cell No.', max_length=20)
+	f_call_back = models.CharField('Fax No.', max_length=20)
+	organization = models.ForeignKey(Group, blank=True, null=True)
+	loss_loc = models.CharField('Loss Location', max_length=400)
+	site_cont = models.CharField('Onsite contact', max_length=100)
+	material_dis = models.CharField('Material Discharge', max_length=100)
+	material_dis_qt = models.CharField('Quantity', max_length=100)
+	pervious = models.BooleanField()
+	impervious = models.BooleanField()
+	surface_water = models.BooleanField()
+	storm_drain = models.BooleanField()
+	msds_sec = models.BooleanField('MSDS secured')
+	dot_reprtbl = models.BooleanField('DOT reportable')
+	c_claim_no = models.CharField('Client Claim #', max_length=100)
+	c_freight_no = models.CharField('Client Freight Bill #', max_length=100)
+	c_adjuster = models.CharField('Adjuster assigned', max_length=100)
+	c_tractor_no = models.CharField('Tractor #', max_length=100)
+	c_trailer_no = models.CharField('Trailer #', max_length=100)
+	c_driver = models.CharField('Driver Name', max_length=100)
+	c_driver_no = models.CharField('Contact Number', max_length=20)
+	police_fire_recovery = models.CharField('Police/Fire/Recovery onsite', max_length=200)
+	agency_state = models.CharField(max_length=100)
+	agency_state_no = models.CharField('Agency State #', max_length=100)
+	agency_epa = models.CharField('Agency EPA', max_length=100)
+	agency_epa_no = models.CharField('Agency EPA #', max_length=100)
+	agency_nrc = models.CharField('Agency NRC', max_length=100)
+	agency_nrc_no = models.CharField('Agency NRC #', max_length=100)
+	responder_info = models.ForeignKey(responder_information, blank=True, null=True)
+	initial_corrective_actions = models.TextField()
+	intial_reserve = models.CharField(max_length=100)
+	ir_given_to = models.CharField('Initial Given to', max_length=100)
+	ir_date = models.DateField('Date')
+	adjusted_reserve = models.CharField(max_length=100)
+	ar_given_to = models.CharField('Adjusted Given to', max_length=100)
+	ar_date = models.DateField('Date')
+	project_scope_timeline = models.TextField()
+	site_work_completed_comments = models.TextField()
+	class Meta:
+		verbose_name = 'Loss Report'
+		verbose_name_plural = 'Loss Reports'
 
